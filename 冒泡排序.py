ff839b6052f7dcd437313177fd3b541b5b5dd676
bobo <45066720+boboxxx@users.noreply.github.com>
2020-06-06 19:15:17 +0800

    maopao

---
+++ 冒泡排序.py
@@ -0,0 +1,10 @@
+import numpy as np 
+x=np.random.randint(1,100,20)
+print("排序前",x)
+for j in range(len(x)-1):
+    for i in range(len(x)-1):
+        if(x[i]>x[i+1]):
+            t=x[i+1]
+            x[i+1]=x[i]
+            x[i]=t
+print("排序后",x)
